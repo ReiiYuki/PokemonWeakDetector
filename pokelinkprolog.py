ddb174bf8f0b875f945059d9a62239aed6f0aeba
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-11 20:22:18 +0700

    Request with pokeapi on mashape

--- pokelinkprolog.py
+++ pokelinkprolog.py
@@ -1,1 +1,9 @@
-import request
+import requests
+
+for i in range(1,719):
+    url ="https://phalt-pokeapi.p.mashape.com/pokemon/"+str(i)+"/"
+    response = requests.get(url,headers={
+        "X-Mashape-Key": "AtHp2pPkyRmshQJuY7FVU6iff6Rgp1HfOZQjsn0sroX3FjwlIU",
+        "Accept": "application/json"
+        })
+    print (response.json())
