d4627c9bbbe080242e0ea96042abf12043ce0852
b5710546232 <nattapat.su@ku.th>
2016-10-14 20:43:59 +0700

    testing engine

--- src/engine.pl
+++ src/engine.pl
@@ -7,3 +7,6 @@ effective_ratio_defense(Attack, [Defense | Tail], Result) :-
     typefact:effective(Attack, Defense, Result1),
     effective_ratio_defense(Attack, Tail, Result2),
     Result is Result1 * Result2.
+    
+# testing ท่าที่ charmander ชนะ bulbasuar rate >= 2
+# pokefact:have_move(charmander,X),movefact:have_type(X,TYPE),pokefact:have_type(bulbasaur,TYPE_E1),pokefact:have_type(bulbasaur,TYPE_E2),not(TYPE_E1=TYPE_E2),effective_ratio_defense(TYPE,[TYPE_E1,TYPE_E2],R),R>=2.
