0b7825af14525420cd7c77f4e5c708e53afd876b
gunhappy <gunhappy123@gmail.com>
2016-10-14 23:17:54 +0700

    vue enemy pic

--- webpage/vue-pokemon/src/App.vue
+++ webpage/vue-pokemon/src/App.vue
@@ -2,66 +2,21 @@
   <div id="app">
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
-    <span>Pokemon1: {{ poke1_selected }}</span>
-    <select v-model="poke1_selected">
-      <option v-for="option in options" v-bind:value="option.text">
-       {{ option.text }}
-     </option>
-    </select>
-    &nbsp;&nbsp;
-    <span>Pokemon2: {{ poke2_selected }}</span>
-    <select v-model="poke2_selected">
-      <option v-for="option in options" v-bind:value="option.text">
-       {{ option.text }}
-     </option>
-    </select>
-    &nbsp;&nbsp;
-    <span>Pokemon3: {{ poke3_selected }}</span>
-    <select v-model="poke3_selected">
-      <option v-for="option in options" v-bind:value="option.text">
-       {{ option.text }}
-     </option>
-    </select>
-    <br><br>
-    <span>Pokemon4: {{ poke4_selected }}</span>
-    <select v-model="poke4_selected">
-      <option v-for="option in options" v-bind:value="option.text">
-       {{ option.text }}
-     </option>
-    </select>
-    &nbsp;&nbsp;
-    <span>Pokemon5: {{ poke5_selected }}</span>
-    <select v-model="poke5_selected">
-      <option v-for="option in options" v-bind:value="option.text">
-       {{ option.text }}
-     </option>
-    </select>
-    &nbsp;&nbsp;
-    <span>Pokemon6: {{ poke6_selected }}</span>
-    <select v-model="poke6_selected">
-      <option v-for="option in options" v-bind:value="option.text">
-       {{ option.text }}
-     </option>
-    </select>
+    <Enemy></Enemy>
   </div>
 </template>
 
 <script>
+
+import Enemy from './components/Enemy.vue'
+
 export default {
+  components:{
+    Enemy: Enemy
+  },
   data () {
     return {
-      msg: 'Enemy Pokemon',
-      poke1_selected: 'A',
-      poke2_selected: 'A',
-      poke3_selected: 'A',
-      poke4_selected: 'A',
-      poke5_selected: 'A',
-      poke6_selected: 'A',
-      options: [
-        { text: 'One', value: 'A' },
-        { text: 'Two', value: 'B' },
-        { text: 'Three', value: 'C' }
-      ]
+      msg: 'Enemy Pokemon'
     }
   }
 }
