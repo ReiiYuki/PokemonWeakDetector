faa26afe3760ed6c9e11d24a81ae84c992360a16
b5710546232 <nattapat.su@ku.th>
2016-10-14 19:57:05 +0700

    testing rule

--- src/engine.pl
+++ src/engine.pl
@@ -1,3 +1,9 @@
-:- use_module(pokefact, []).
-:- use_module(typefact, []).
+:- use_module(pokefact,[]).
 :- use_module(movefact,[]).
+:- use_module(typefact,[]).
+
+effective_ratio_defense(_, [], 1.0).
+effective_ratio_defense(Attack, [Defense | Tail], Result) :-
+    typefact:effective(Attack, Defense, Result1),
+    effective_ratio_defense(Attack, Tail, Result2),
+    Result is Result1 * Result2.
