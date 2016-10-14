4574cac994ca908b962d42a1abe813d93c971dbb
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-14 18:31:25 +0700

    Move Fact

--- poke_move_fact_writer.py
+++ poke_move_fact_writer.py
@@ -1,5 +1,16 @@
 import json
 with open('moves_type.json') as poke_data:
+    move_list = []
+    type_list = []
     d = json.load(poke_data)
     for i in d :
-        print(i['name'])
+        move = i['move_name']
+        type = i['type']
+        move_list.append('move('+move.lower()+').')
+        type_list.append('have_type('+move.lower()+','+type.lower()+').')
+    out = open('movefact.pl','w')
+    for i in type_list :
+        out.write(i+'\n')
+    for i in move_list :
+        out.write(i+'\n')
+    out.close()
