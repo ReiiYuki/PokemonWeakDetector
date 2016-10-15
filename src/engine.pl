8420254f705fd585946e461f1518789a5eb158db
paiizz <wanchanapon.t@ku.th>
2016-10-15 14:51:14 +0700

    edit rule engine.pl

--- src/engine.pl
+++ src/engine.pl
@@ -19,9 +19,10 @@
 beat(P,M,E):-pokefact:pokemon(P),pokefact:have_move(P,M),
   movefact:move(M),movefact:have_type(M,T),
   pokefact:have_type(E,T1),
-  typefact:effective(T,T1,R),R>=2.
+  typefact:effective(T,T1,R),R>=2,
+  stronger(P,E).
 
-stronger(A,B):-statfact:have_base_stat(A,Stat1),statfact:have_base_stat(B,Stat2),abs(Stat1-Stat2)>=50.
+stronger(A,B):-statfact:have_base_stat(A,Stat1),statfact:have_base_stat(B,Stat2),(Stat1-Stat2)>=50.
 
 
 
