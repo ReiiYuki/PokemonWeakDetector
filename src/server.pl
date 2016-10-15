48e8db470041aedd32628228c11ca0014a1d71a7
ReiiYuki <iguru.shou.5410070@gmail.com>
2016-10-16 00:01:15 +0700

    Complete JSON Restful

--- src/server.pl
+++ src/server.pl
@@ -5,16 +5,22 @@
 :- use_module(library(http/http_dispatch)).
 
 :- use_module(library(http/http_json)).
-
+:- use_module(library(http/json_convert)).
 :- http_handler(root(.),handle,[]).
 
 server(Port) :-
    http_server(http_dispatch,[port(Port)]).
-
+:- json_object
+        poke(pokemon:text, move:text).
 handle(Request) :-
    http_read_json_dict(Request, DictIn,[json_object(term)]),
-   % pokerule:beat(P,M,DictIn.pokemon),
-   % format(user_output,"Pokemon is: ~p~n",[P]),
-   % format(user_output,"Move is: ~p~n",[M]),
-   DictOut=DictIn,
+   format(user_output,"I'm here~n",[]),
+   term_string(Pokemon,DictIn.pokemon),
+   findall(poke(P,M),beat(P,M,Pokemon),L),
+   format(user_output,"Pokemons are: ~p~n",[L]),
+   prolog_to_json(L,J),
+   DictOut=J,
    reply_json(DictOut).
+
+beat(P,M,E) :-
+   pokerule:beat(P,M,E).
