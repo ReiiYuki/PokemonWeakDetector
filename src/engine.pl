ec24874f600af2c4ad9fb0fb235dee922e53a2a9
paiizz <wanchanapon.t@ku.th>
2016-10-15 16:07:40 +0700

    modify rule

--- src/engine.pl
+++ src/engine.pl
@@ -9,15 +9,15 @@
 %
 % effective_ratio_defense(_, [], 1.0).
 % effective_ratio_defense(Attack, [Defense | Tail], Result) :-
-%     not(Defense = Tail),
-%     typefact:effective(Attack, Defense, Result1),
-%     effective_ratio_defense(Attack, Tail, Result2),
-%     Result is Result1 * Result2.
+% not(Defense = Tail),
+% typefact:effective(Attack, Defense, Result1),
+% effective_ratio_defense(Attack, Tail, Result2),
+% Result is Result1 * Result2.
 
 % pokefact:have_move(charmander,X),movefact:have_type(X,TYPE),pokefact:have_type(bulbasaur,TYPE_E1),pokefact:have_type(bulbasaur,TYPE_E2),not(TYPE_E1=TYPE_E2),effective_ratio_defense(TYPE,[TYPE_E1,TYPE_E2],R),R>=2.
 
 % movefact:have_type(dragon-tail,T),pokefact:have_type(abra,T1),effective(T,T1,R),R>=2.
 % pokefact:pokemon(pikachu),
-%  pokefact:have_move(pikachu,ME),
-%  movefact:have_type(ME,TE),
-%  typefact:effective(TE,water,R),R=<1.
+% pokefact:have_move(pikachu,ME),
+% movefact:have_type(ME,TE),
+% typefact:effective(TE,water,R),R=<1.
--- src/pokerule.pl
+++ src/pokerule.pl
@@ -1,9 +1,11 @@
 :- module(pokerule, []).
 
-beat(P,M,E):-pokefact:pokemon(P),pokefact:have_move(P,M),
+pokerule:beat(P,M,E):-pokefact:pokemon(P),pokefact:have_move(P,M),
   movefact:move(M),movefact:have_type(M,T),
   pokefact:have_type(E,T1),
   typefact:effective(T,T1,R),R>=2,
   stronger(P,E).
 
-stronger(A,B):-statfact:have_base_stat(A,Stat1),statfact:have_base_stat(B,Stat2),(Stat1-Stat2)>=50.
+% pokerule:stronger(A,B):-
+
+pokerule:same_level(A,B) :- statfact:have_base_stat(A,Stat1),statfact:have_base_stat(B,Stat2),((Stat1-Stat2)>=50 ; (Stat1-Stat2)=<50).
