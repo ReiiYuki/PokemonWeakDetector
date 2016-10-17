06f9ca57b295beaef821d0967440336d6b18cd70
b5710546232 <nattapat.su@ku.th>
2016-10-18 03:23:04 +0700

    App vue

--- webpage/vue-pokemon/src/App.vue
+++ webpage/vue-pokemon/src/App.vue
@@ -4,7 +4,7 @@
     </div>
     <div style="position: fixed; z-index: -99; width: 100%; opacity: .25; height: 100%">
   <iframe frameborder="0" height="100%" width="100%"
-    src="https://youtube.com/embed/d3YI3NYywg0?autoplay=1&controls=0&showinfo=0&autohide=1">
+    src="https://youtube.com/embed/Nmipdds2PzI?autoplay=1&controls=0&showinfo=0&autohide=1&loop=1">
     <!-- 6xKWiCMKKJg -->
     <!-- d3YI3NYywg0 list -->
     <!-- Nmipdds2PzI -->
